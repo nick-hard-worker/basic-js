3756801588dd0c01d127eedea335201d5d8699a5
NickUA <246246s@gmail.com>
2020-10-13 21:04:01 +0300

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
+  // createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
+  if (!Array.isArray(members)) return false;
+
+  return members
+    .filter(item => typeof (item) === 'string')
+    .map(item => item.replace(/^\s+/, ''))
+    .map(item => item[0].toUpperCase())
+    .sort()
+    .join('')
 };
