9700d126d6386a9e2f73e1fed500bcf7ca1797e7
NickUA <246246s@gmail.com>
2020-10-14 22:11:01 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // throw new CustomError('Not implemented');
+  if (!Array.isArray(arr)) throw new Error();
+  if (arr.length === 0) return [];
+
+  const resultArr = [];
+  let i = 0;
+  while (i <= (arr.length - 1)) {
+
+    switch (arr[i]) {
+      case '--discard-next': i++;
+        break;
+      case '--discard-prev':
+        if (i === 0) { i++; continue; }
+        //discardDiscarded:
+        if (arr[i - 2] === '--discard-next') { i++; continue };
+        resultArr.pop();
+        break;
+      case '--double-next':
+        if (i === arr.length - 1) { i++; continue; }
+        resultArr.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        //doubleDiscarded or first item
+        if (arr[i - 2] === '--discard-next' || i === 0) { i++; continue };
+
+        resultArr.push(arr[i - 1]);
+        break;
+      default:
+        resultArr.push(arr[i]);
+        break;
+    }
+
+    i++
+  }
+  return resultArr;
+
 };
